498f077a81e1ea2609805ae8c3378104cdd6784a
Tuhin35 <ariaan59@gmail.com>
2023-05-17 15:56:58 +0600

    update json file

---
+++ public/Mackup.js
@@ -0,0 +1,50 @@
+const makeupArtists = [
+    {
+      name: "Makeup Artist 1",
+      priceRating: 4.5,
+      agency: {
+        name: "Agency 1",
+        websiteURL: "http://agency1.com",
+        location: {
+          address: "123 Main Street",
+          district: "City Center"
+        },
+        contact: {
+          phone: "123-456-7890",
+          email: "contact@agency1.com"
+        }
+      },
+      makeupServices: {
+        bridalMakeupPicture: "https://cdn.pixabay.com/photo/2022/03/05/09/34/woman-7048766_960_720.jpg",
+        birthdayMakeupPicture: "https://cdn.pixabay.com/photo/2018/02/16/17/56/outdoors-3158172_640.jpg",
+        partyMakeupPicture: "https://cdn.pixabay.com/photo/2018/03/12/20/57/portrait-3220832_640.jpg"
+      }
+    },
+    {
+      name: "Makeup Artist 2",
+      priceRating: 3.8,
+      pictures: [
+        "picture4.jpg",
+        "picture5.jpg",
+        "picture6.jpg"
+      ],
+      agency: {
+        name: "Agency 2",
+        websiteURL: "http://agency2.com",
+        location: {
+          address: "456 Elm Street",
+          district: "Suburb"
+        },
+        contact: {
+          phone: "987-654-3210",
+          email: "contact@agency2.com"
+        }
+      },
+      makeupServices: {
+        bridalMakeupPicture: "https://cdn.pixabay.com/photo/2019/06/06/11/53/model-4255847_640.jpg",
+        birthdayMakeupPicture: "https://cdn.pixabay.com/photo/2019/04/16/15/38/indian-4132033_640.jpg",
+        partyMakeupPicture: "https://cdn.pixabay.com/photo/2017/06/18/23/16/girl-2417575_640.jpg"
+      }
+    }
+  ];
+  
