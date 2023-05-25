3771e1a78d6f9d6226ae2cfcbe1d66a1e9e13fd4
Tuhin35 <ariaan59@gmail.com>
2023-05-25 09:35:57 +0600

    added

--- public/Cloth.js
+++ public/Cloth.js
@@ -1,35 +1,82 @@
-const cloth = [
+const clothes = [
+    // Bride Clothes
     {
-      clothName: "T-Shirt",
+      category: "Bride",
+      clothName: "Wedding Gown",
       brandName: "Brand A",
-      rating: 4.2,
-      picture: "tshirt.jpg",
-      price: 29.99,
+      rating: 4.7,
+      picture: "bride_gown.jpg",
+      price: 999.99,
+      size: "S"
+    },
+    {
+      category: "Bride",
+      clothName: "Bridal Lehenga",
+      brandName: "Brand B",
+      rating: 4.5,
+      picture: "bridal_lehenga.jpg",
+      price: 1499.99,
+      size: "M"
+    },
+    
+    // Groom Clothes
+    {
+      category: "Groom",
+      clothName: "Tuxedo",
+      brandName: "Brand C",
+      rating: 4.8,
+      picture: "groom_tuxedo.jpg",
+      price: 799.99,
       size: "M"
     },
     {
-      clothName: "Jeans",
-      brandName: "Brand B",
-      rating: 4.8,
-      picture: "jeans.jpg",
-      price: 59.99,
-      size: "32/34"
+      category: "Groom",
+      clothName: "Sherwani",
+      brandName: "Brand D",
+      rating: 4.6,
+      picture: "groom_sherwani.jpg",
+      price: 1199.99,
+      size: "L"
     },
+    
+    // Party Clothes
     {
-      clothName: "Dress",
-      brandName: "Brand C",
-      rating: 3.9,
-      picture: "dress.jpg",
-      price: 79.99,
+      category: "Party",
+      clothName: "Evening Dress",
+      brandName: "Brand E",
+      rating: 4.3,
+      picture: "party_dress.jpg",
+      price: 199.99,
       size: "L"
     },
     {
-      clothName: "Shirt",
-      brandName: "Brand D",
-      rating: 4.5,
-      picture: "shirt.jpg",
-      price: 39.99,
+      category: "Party",
+      clothName: "Cocktail Dress",
+      brandName: "Brand F",
+      rating: 4.1,
+      picture: "cocktail_dress.jpg",
+      price: 149.99,
       size: "S"
+    },
+    
+    // Birthday Clothes
+    {
+      category: "Birthday",
+      clothName: "Casual Outfit",
+      brandName: "Brand G",
+      rating: 4.2,
+      picture: "birthday_outfit.jpg",
+      price: 89.99,
+      size: "XL"
+    },
+    {
+      category: "Birthday",
+      clothName: "Party Dress",
+      brandName: "Brand H",
+      rating: 4.0,
+      picture: "birthday_party_dress.jpg",
+      price: 99.99,
+      size: "M"
     }
   ];
   
